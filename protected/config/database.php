0e0877afa6993a36f0bf30db806d903609e76c3f
selomdev <selomdev@gmail.com>
2016-12-09 15:59:58 +0000

    Modification database name

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=pharmatie projet',
+	'connectionString' => 'mysql:host=localhost;dbname=pharmatie_projet',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
