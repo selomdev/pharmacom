025b4163ea5bd3e0e78c234e42c1d707ac3aa218
Moumine Arthur <Moumine Arthur@192.168.0.72>
2016-12-14 09:01:05 +0000

    Ajout de la fonction addPharmacie dans le controleur pharmatieController

--- protected/controllers/pharmatieController.php
+++ protected/controllers/pharmatieController.php
@@ -4,19 +4,73 @@ class PharmatieController extends Controller{
 /**
 ** Fonction retournant tous les donnees contenues dans la table pharmatie
 **/
- public function actionAllPharmatie()
+ public function actionAllPharmacie()
 {
  $i=0;
  $pharms=[];
    $Parmaties = PharmatieEnligne::model()->findAll();
-  $result= json_encode($Parmaties); 
+  $result= json_encode($Parmaties);
+  $data=array();
+
 foreach($Parmaties as $phar){
-    $pharms[$i]=$phar->attributes;
+
+    $pharms[$i]= $phar->attributes;
  
 $i++;
 }
-
+    foreach ($pharms as $pharm=>$value) {
+    $champs=array();
+    foreach ($value as $key=>$v){
+        $champs[$key]=($v);
+    }
+        $data[$pharm]=$champs;
+    }
+    header("Access-Control-Allow-Origin:*");
+    header("Content-Type:application/json;charset=utf-8");
+//echo(json_encode($data));
   echo json_encode($pharms);
+
+}
+
+    public function actionAddPharmacie()
+    {
+        $model = new PharmatieEnligne();
+        $postData=file_get_contents("php://input");
+         if(isset($_SERVER['HTTP_ORIGIN'])){
+    header("Access-Control-Allow-Origin:{$_SERVER['HTTP_ORIGIN']}");
+           // header("Access-Control-Allow-Origin:*");
+    header("Access-Control-Allow-Credentials:true");
+    header("Access-Control-Max-Age:86400");
+}
+if($_SERVER['REQUEST_METHOD']=='OPTIONS'){
+            if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header("Access-Control-Allow-Methods:GET,POST,OPTIONS");
+            if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allows-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            exit(0);
+}
+
+
+if (isset($postData)){
+    $request = json_decode($postData);
+
+    //$username = $request->nom_pharmaLign;
+    foreach ($request as $k=>$v){
+        $model->$k = $v;
+    }
+    if($model->save()){
+        echo json_encode("Success");
+    }
+    else{
+        echo json_encode("Error");
+    }
+}
+else{
+    echo "Not called properly with username!";
+}
+
+
+
 
 }
 
